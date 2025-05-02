986d9a705ff9aeb2e6e32a0cdc4a976f7fa54d27
Tony Rutherford <ar56058@gmail.com>
2025-05-01 21:01:11 -0500

    CSA implemented

--- algorithms.py
+++ algorithms.py
@@ -2,22 +2,6 @@ import itertools
 from transit import TransitGrid, TransitNode
 import heapq
 
-class GraphNode:
-    def __init__(self, station: TransitNode, arrival_time: float, actions = []):
-        self.station = station
-        self.edges = []
-
-        # arrival_time is the same as total cost to get to the node through this edge, but is
-        # needed to fetch bus departure times
-        self.arrival_time = arrival_time
-        self.actions = actions
-
-class GraphEdge:
-    def __init__(self, from_node: TransitNode, to_node: TransitNode, weight: float):
-        self.from_node = from_node
-        self.to_node = to_node
-        self.weight = weight
-
 class Algorithm:
     
     def __init__(self):
@@ -59,12 +43,10 @@ class DijkstraAlgorithm(Algorithm):
 
         pass
 
-    def expand_node(self, root_node: GraphNode):
-
-        for nbr in root_node.station.neighbor_stations:
-            root_node.edges.append(GraphEdge(root_node, nbr, self.get_transfer_time(root_node.station, nbr, root_node.arrival_time)))
-
     def get_path(self, grid: TransitGrid):
+
+        if grid.passenger.cur_pos == grid.passenger.goal_pos:
+            return [], 0
 
         (start_x, start_y) = grid.passenger.cur_pos
         start_station = grid.get_from_grid(start_x, start_y)
@@ -117,10 +99,121 @@ class DijkstraAlgorithm(Algorithm):
 
         return None, float('inf')
         
+import copy
 
 class ConnectionAlgorithm(Algorithm):
     def __init__(self):
         pass
 
+    CYCLES_REQUIRED_CONSTANT = 2.5
+
+    # takes advantage of the fact that bus movement is cyclic
+    # and repeats every (size-1)*2 minutes
+    def get_connections_list(self, grid:TransitGrid):
+        # stores (fromStation:coordinate, departureTime:float, toStation:coordinate, arrivalTime:float)
+        connections: list[tuple[tuple[int,int], float, tuple[int,int], float]] = []
+
+        routes = grid.horizontal_routes + grid.vertical_routes
+
+        # simulate 4 cycles for each route
+        for route in routes:
+            sim_time = 0
+            cycle_length = (grid.size-1)*2
+
+            # EX: if we store that we visited station A when sim_time = 5,
+            # and we encounter station B when sim_time = 7, we have enough info to
+            # populate a connection. then if we encounter station C when sim_time = 10, we once again
+            # can populate connections, this time for A->C and B->C
+            station_visit_times = {}
+
+            bus_pos = route.bus_pos
+            bus_dir = route.bus_direction
+
+            def simulate_step(bus_pos, bus_dir, sim_time):
+                (dx, dy) = route.get_movement(bus_dir)
+                bus_pos, bus_dir = route.move_bus(bus_pos, bus_dir, dx, dy)
+
+                return bus_pos, bus_dir, sim_time + 0.5
+
+            while sim_time < cycle_length * self.CYCLES_REQUIRED_CONSTANT:
+
+                cur_station = grid.get_from_grid(bus_pos[0], bus_pos[1])
+
+                if not cur_station or not cur_station.is_station:
+                    bus_pos, bus_dir, sim_time = simulate_step(bus_pos, bus_dir, sim_time)
+                    continue
+
+                cur_pos = (bus_pos[0], bus_pos[1])
+
+                for pos,dep_time in station_visit_times.items():
+
+                    # CSA dictates that connections with identical dep and arr stations are invalid
+                    if pos == cur_pos:
+                        continue
+
+                    connections.append((pos, dep_time, cur_station.get_pos(), sim_time))
+
+                station_visit_times[cur_pos] = sim_time
+
+                bus_pos, bus_dir, sim_time = simulate_step(bus_pos, bus_dir, sim_time)
+            
+        # sort connections by departure time
+        connections.sort(key=lambda c: c[1])
+
+        return connections
+
+                
+
+
+
+    #def offset_connections_list(self, grid:TransitGrid):
+
+
     def get_path(self, grid: TransitGrid):
-        pass
+
+        if grid.passenger.cur_pos == grid.passenger.goal_pos:
+            return [], 0
+
+        # stores (fromStation:TransitNode, departureTime:float, toStation:TransitNode, arrivalTime:float)
+        connections = self.get_connections_list(grid)
+
+        # map each station to earliest arrival time, defaulting to 'inf'
+        earliest_arrival = {pos: float('inf') for (dep_pos, dep_time, arr_pos, arr_time) in connections for pos in [dep_pos, arr_pos]}
+        earliest_arrival[grid.passenger.cur_pos] = grid.time
+
+        # needed to reconstruct the path
+        predecessors = {}
+
+        for (dep_pos, dep_time, arr_pos, arr_time) in connections:
+            # if dep_time is after the earliest arrival to the station,
+            # connection can be taken
+            if dep_time >= earliest_arrival[dep_pos]:
+                # if the connection would lead to an earlier arrival time at
+                # the station, update the earliest arrival and predeccesor
+                if arr_time < earliest_arrival[arr_pos]:
+                    earliest_arrival[arr_pos] = arr_time
+                    predecessors[arr_pos] = (dep_pos, dep_time, arr_pos, arr_time)
+
+        goal = grid.passenger.goal_pos
+
+        if goal not in predecessors:
+            raise ValueError("No path found!")
+
+        actions = []
+        cur_pos = goal
+
+        while cur_pos in predecessors:
+            (dep_pos, dep_time, arr_pos, arr_time) = predecessors[cur_pos]
+
+            actions.append((arr_time, "unboard"))
+
+            if dep_pos[0] == arr_pos[0]:
+                actions.append((dep_time, "boardVert"))
+            else:
+                actions.append((dep_time, "boardHoriz"))
+
+            cur_pos = dep_pos
+
+        actions.reverse()
+
+        return actions, earliest_arrival[goal]
--- main.py
+++ main.py
@@ -6,16 +6,6 @@ import os
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def run_dijkstras_test(grid: TransitGrid): # TODO
-
-    dijkstra = DijkstraAlgorithm()
-    raptor = RaptorAlgorithm()
-    drt = DrtAlgorithm()
-
-    results = [benchmark_algo(dijkstra, grid), benchmark_algo(raptor, grid), benchmark_algo(drt, grid)]
-
-    print(results)
-
 def run_algo(algo: Algorithm, grid: TransitGrid): # TODO
 
     before_time = time.time()
@@ -43,7 +33,7 @@ def run_algo(algo: Algorithm, grid: TransitGrid): # TODO
     after_time = time.time()
 
     # returns elapsed time
-    return after_time - before_time
+    return after_time - before_time, total_sim_time
 
 def run_full_test(repetitions = 1000, grid_size = 10, express_chance = 0.2, random_bus_start=True, n_stations = 50): #TODO: this should run many tests at the same time
 
@@ -53,32 +43,73 @@ def run_full_test(repetitions = 1000, grid_size = 10, express_chance = 0.2, rand
 
     times_seconds = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
 
+    max_sim_times = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
+
     for i in range(repetitions):
 
         seed = random.randint(0, 100000)
 
         grid_dij = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
-        times_seconds["dijkstras"] += run_algo(dijkstra, grid_dij)
+        
+        elapsed_time_dij, sim_time_dij = run_algo(dijkstra, grid_dij)
+        
+        times_seconds["dijkstras"] += elapsed_time_dij
+        max_sim_times["dijkstras"] = max(max_sim_times["dijkstras"], sim_time_dij)
 
         # TODO: uncomment once implemented
         # grid_rapt = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
         # times_seconds["raptor"] += run_algo(raptor, grid_rapt)
 
         # TODO: uncomment once implemented
-        # grid_conn = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
-        # times_seconds["connection"] += run_algo(connection, grid_conn)
+        grid_conn = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
+        
+        elapsed_time_conn, sim_time_conn = run_algo(connection, grid_conn)
+        times_seconds["connection"] += elapsed_time_conn
+        max_sim_times["connection"] = max(sim_time_conn, max_sim_times["connection"])
 
-    return times_seconds
+        if sim_time_dij != sim_time_conn:
+            raise ValueError("Values should be equal for optimal solutions")
+
+    return times_seconds, max_sim_times
+
+# small grid size
 
 repetitions = 1000
 grid_size = 10
 express_chance = 0.2
-n_stations = 50
+n_stations = grid_size*grid_size // 2
 
-test_results = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
+test_results, max_sim_times = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
 print("Algorithm timing results for " + str(repetitions) + " repetitions of a " + str(grid_size) + "x" + str(grid_size) 
       + " grid with a " + str(express_chance) + " probability of express lines and " + str(n_stations) + " stations:")
 print(test_results)
+print(max_sim_times)
+
+# medium grid size
+
+# repetitions = 100
+# grid_size = 25
+# express_chance = 0.2
+# n_stations = grid_size*grid_size // 2
+
+# test_results, max_sim_times = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
+# print("Algorithm timing results for " + str(repetitions) + " repetitions of a " + str(grid_size) + "x" + str(grid_size) 
+#       + " grid with a " + str(express_chance) + " probability of express lines and " + str(n_stations) + " stations:")
+# print(test_results)
+# print(max_sim_times)
+
+# large grid size
+
+# repetitions = 100
+# grid_size = 50
+# express_chance = 0.2
+# n_stations = grid_size*grid_size // 2
+
+# test_results, max_sim_times = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
+# print("Algorithm timing results for " + str(repetitions) + " repetitions of a " + str(grid_size) + "x" + str(grid_size) 
+#       + " grid with a " + str(express_chance) + " probability of express lines and " + str(n_stations) + " stations:")
+# print(test_results)
+# print(max_sim_times)
 
 # UNCOMMENT TO TEST DIJKSTRAS INCLUDING PRINTING INTERMEDIATE GRIDS
 # dij = DijkstraAlgorithm()
