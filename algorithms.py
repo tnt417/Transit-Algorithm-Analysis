1c82fdb58ff4724c198293d088b778553a0feb69
Tony Rutherford <ar56058@gmail.com>
2025-05-03 21:51:40 -0500

    New working raptor

--- algorithms.py
+++ algorithms.py
@@ -1,41 +1,42 @@
 import itertools
+import math
 from transit import TransitGrid, TransitNode, TransitRoute
 import heapq
 import sys
 
 
-class RaptorRoute:
-    def __init__(self):
-        self.stops = []
-        self.trip_interval = 0
+# class RaptorRoute:
+#     def __init__(self):
+#         self.stops = []
+#         self.trip_interval = 0
 
-    def __str__(self):
-        return f"RaptorRoute: <interval: {self.interval}, stops: {self.stops}>"
-
-
-class RaptorPosition:
-    def __init__(self, pos):
-        self.x = pos[0]
-        self.y = pos[1]
-        self.routes = set()
-        self.stops = set()
-        self.times_by_round = {}  # Allows for easy gaps, I am lazy
-        self.earliest_time = sys.maxsize
+#     def __str__(self):
+#         return f"RaptorRoute: <interval: {self.interval}, stops: {self.stops}>"
 
 
-class RaptorScheduledStop:
-    def __init__(self, x, y, t, route):
-        # All routes can be transferred between in 0.1 (above 0) time units
-        self.x = x
-        self.y = y
-        self.t = t
-        self.route = route
+# class RaptorPosition:
+#     def __init__(self, pos):
+#         self.x = pos[0]
+#         self.y = pos[1]
+#         self.routes = set()
+#         self.stops = set()
+#         self.times_by_round = {}  # Allows for easy gaps, I am lazy
+#         self.earliest_time = sys.maxsize
 
-    def get_pos(self):
-        return (self.x, self.y)
 
-    def __str__(self):
-        return f"RaptorStop: <x: {self.x}, y: {self.y}, t: {self.t}, rounds: {self.times_by_round}, earliest: {self.earliest_time}"
+# class RaptorScheduledStop:
+#     def __init__(self, x, y, t, route):
+#         # All routes can be transferred between in 0.1 (above 0) time units
+#         self.x = x
+#         self.y = y
+#         self.t = t
+#         self.route = route
+
+#     def get_pos(self):
+#         return (self.x, self.y)
+
+#     def __str__(self):
+#         return f"RaptorStop: <x: {self.x}, y: {self.y}, t: {self.t}, rounds: {self.times_by_round}, earliest: {self.earliest_time}"
 
 
 class Algorithm:
@@ -46,126 +47,132 @@ class Algorithm:
         pass
 
 
-class RaptorAlgorithm(Algorithm):
-    def __init__(self):
-        self.dijkstra = DijkstraAlgorithm() # FIXME: Remove
+# class RaptorAlgorithm(Algorithm):
+#     def __init__(self):
+#         self.dijkstra = DijkstraAlgorithm() # FIXME: Remove
 
-    def get_trip_from_stop_time(self, route: RaptorRoute, time: int):
-        return int(time % (route.trip_interval))
+#     def get_trip_from_stop_time(self, route: RaptorRoute, time: int):
+#         return int(time % (route.trip_interval))
 
-    def get_time_from_stop_and_trip(self, route: RaptorRoute, stop: RaptorScheduledStop, trip_num: int):
-        return stop.t + trip_num * route.trip_interval
+#     def get_time_from_stop_and_trip(self, route: RaptorRoute, stop: RaptorScheduledStop, trip_num: int):
+#         return stop.t + trip_num * route.trip_interval
 
-    def get_earliest_trip(self, route: RaptorRoute, stop: RaptorScheduledStop, time: int):
-        return int((time - stop.t) % route.trip_interval)
+#     def get_earliest_trip(self, route: RaptorRoute, stop: RaptorScheduledStop, time: int):
+#         return math.ceil((time - stop.t) / route.trip_interval)
 
-    def build_route_data(self, route: TransitRoute):
-        route_data = RaptorRoute()
-        step = 0.5 if route.is_express else 1
-        t = 0
-        for station in route.stations:
-            route_data.stops.append(RaptorScheduledStop(
-                station.pos_X,
-                station.pos_Y,
-                t,
-                route_data))
-            t += step
+#     def build_route_data(self, route: TransitRoute):
+#         route_data = RaptorRoute()
+#         step = 0.5 if route.is_express else 1
+#         t = 0
+#         for station in route.stations:
+#             route_data.stops.append(RaptorScheduledStop(
+#                 station.pos_X,
+#                 station.pos_Y,
+#                 t,
+#                 route_data))
+#             t += step
 
-        # Create the reverse direction's timetable
-        for stop in reversed(route_data.stops):
-            route_data.stops.append(RaptorScheduledStop(
-                stop.x,
-                stop.y,
-                t,
-                route_data))
-            t += step
+#         # Create the reverse direction's timetable
+#         for stop in reversed(route_data.stops):
+#             route_data.stops.append(RaptorScheduledStop(
+#                 stop.x,
+#                 stop.y,
+#                 t,
+#                 route_data))
+#             t += step
 
-        route_data.trip_interval = t
-        return route_data
+#         route_data.trip_interval = t
+#         return route_data
 
-    def get_raptor_data(self, grid: TransitGrid):
-        routes = set()
-        pos_data = {}
-        for route in itertools.chain(grid.vertical_routes, grid.horizontal_routes):
-            route_data = self.build_route_data(route)
-            routes.add(route_data)
+#     def get_raptor_data(self, grid: TransitGrid):
+#         routes = set()
+#         pos_data = {}
+#         for route in itertools.chain(grid.vertical_routes, grid.horizontal_routes):
+#             route_data = self.build_route_data(route)
+#             routes.add(route_data)
 
-            for stop in route_data.stops:
-                pos = stop.get_pos()
-                if pos not in pos_data:
-                    pos_data[pos] = RaptorPosition(pos)
-                pos_data[pos].routes.add(route)
-                pos_data[pos].stops.add(stop)
-        return (routes, pos_data)
+#             for stop in route_data.stops:
+#                 pos = stop.get_pos()
+#                 if pos not in pos_data:
+#                     pos_data[pos] = RaptorPosition(pos)
+#                 pos_data[pos].routes.add(route)
+#                 pos_data[pos].stops.add(stop)
 
-    def generate_actions_from_path(self, grid): # FIXME: Fix arguments
-        # FIXME: Reassemble properly
-        # Use dijkstra for now so it does not break
-        # Reference: https://ljn.io/posts/raptor-journey-planning-algorithm
-        return self.dijkstra.get_path(grid)
+#         return (routes, pos_data)
 
-    def get_path(self, grid: TransitGrid):
-        route_data, pos_data = self.get_raptor_data(grid)
-        start_pos = grid.passenger.cur_pos
+#     def generate_actions_from_path(self, grid): # FIXME: Fix arguments
+#         # FIXME: Reassemble properly
+#         # Use dijkstra for now so it does not break
+#         # Reference: https://ljn.io/posts/raptor-journey-planning-algorithm
+#         return self.dijkstra.get_path(grid)
 
-        end_pos = grid.passenger.goal_pos
+#     def get_path(self, grid: TransitGrid):
+#         route_data, pos_data = self.get_raptor_data(grid)
+#         start_pos = grid.passenger.cur_pos
 
-        round_num = 0
-        marked_positions = [start_pos]
+#         end_pos = grid.passenger.goal_pos
 
-        while len(marked_positions) > 0:
-            round_num += 1
-            Q = {}
+#         round_num = 0
+#         marked_positions = [start_pos]
 
-            for pos in marked_positions:
-                for stop in pos_data[pos].stops:
-                    r = stop.route
-                    if (r in Q and Q[r].t < stop.t) or r not in Q:
-                        Q[r] = stop
+#         pos_data[start_pos].earliest_time = 0
+#         pos_data[start_pos].times_by_round[0] = 0
 
-            marked_positions = []  # Clear all markings
+#         #print(route_data)
 
-            for route, stop in Q.items():
-                trip = None
-                found_start = False
-                for route_stop in route.stops:
-                    if not (found_start or
-                            route_stop.x != stop.x or
-                            route_stop.y != stop.y):
-                        continue
+#         while len(marked_positions) > 0:
+#             round_num += 1
+#             Q = {}
 
-                    pos = stop.get_pos()
-                    if trip is not None:
-                        earliest_stop_time = pos_data[stop.get_pos()].earliest_time
-                        earliest_target_time = pos_data[end_pos].earliest_time
-                        if earliest_stop_time is None:
-                            earliest_stop_time = sys.maxsize
-                        if earliest_target_time is None:
-                            earliest_target_time = sys.maxsize
+#             for pos in marked_positions:
+#                 for stop in pos_data[pos].stops:
+#                     r = stop.route
+#                     if (r in Q and Q[r].t < stop.t) or r not in Q:
+#                         Q[r] = stop
 
-                        arrive_time = self.get_time_from_stop_and_trip(
-                            route,
-                            route_stop,
-                            trip)
-                        if arrive_time < min(earliest_stop_time,
-                                             earliest_target_time):
-                            pos_data[pos].earliest_time = arrive_time
-                            pos_data[pos].times_by_round[round_num] = arrive_time
-                            marked_positions.append(pos)
+#             marked_positions = []  # Clear all markings
 
-                    cur_time = pos_data[pos].times_by_round.get(round_num)
-                    prev_time = pos_data[pos].times_by_round.get(round_num - 1)
-                    if cur_time is None:
-                        cur_time = sys.maxsize
-                    if prev_time is None:
-                        prev_time = sys.maxsize
-                    if prev_time <= cur_time:
-                        trip = self.get_earliest_trip(route, stop, prev_time)
+#             for route, stop in Q.items():
+#                 trip = None
+#                 found_start = False
+#                 for route_stop in route.stops:
+#                     if not (found_start or
+#                             route_stop.x != stop.x or
+#                             route_stop.y != stop.y):
+#                         continue
 
-            # We skip the footpath stage since all transfers are assumed to be
-            # the same station. Talk about this in our report as a limitation
+#                     pos = stop.get_pos()
+#                     if trip is not None:
+#                         earliest_stop_time = pos_data[stop.get_pos()].earliest_time
+#                         earliest_target_time = pos_data[end_pos].earliest_time
+#                         if earliest_stop_time is None:
+#                             earliest_stop_time = sys.maxsize
+#                         if earliest_target_time is None:
+#                             earliest_target_time = sys.maxsize
 
-        return self.generate_actions_from_path(grid)
+#                         arrive_time = self.get_time_from_stop_and_trip(
+#                             route,
+#                             route_stop,
+#                             trip)
+#                         if arrive_time < min(earliest_stop_time,
+#                                              earliest_target_time):
+#                             pos_data[pos].earliest_time = arrive_time
+#                             pos_data[pos].times_by_round[round_num] = arrive_time
+#                             marked_positions.append(pos)
+
+#                     cur_time = pos_data[pos].times_by_round.get(round_num)
+#                     prev_time = pos_data[pos].times_by_round.get(round_num - 1)
+#                     if cur_time is None:
+#                         cur_time = sys.maxsize
+#                     if prev_time is None:
+#                         prev_time = sys.maxsize
+#                     if prev_time <= cur_time:
+#                         trip = self.get_earliest_trip(route, stop, prev_time)
+
+#             # We skip the footpath stage since all transfers are assumed to be
+#             # the same station. Talk about this in our report as a limitation
+
+#         return self.generate_actions_from_path(grid)
 
 
 class DijkstraAlgorithm(Algorithm):
@@ -367,3 +374,118 @@ class ConnectionAlgorithm(Algorithm):
         actions.reverse()
 
         return actions, earliest_arrival[goal]
+
+class RaptorAlgorithm(Algorithm):
+    def __init__(self, max_rounds: int = 10):
+        self.max_rounds = max_rounds
+
+    def get_path(self, grid: TransitGrid):
+
+        # print(grid)
+
+        start_pos = grid.passenger.cur_pos
+        goal_pos = grid.passenger.goal_pos
+        time = grid.time
+
+        if start_pos == goal_pos:
+            return [], time
+
+        # arrival times for each round
+        arrival_times = [{s.get_pos(): float('inf') for s in grid.stations}
+                         for _ in range(self.max_rounds + 1)]
+        
+        # arrive at the start pos at time = 0
+        arrival_times[0][start_pos] = time
+
+        # track predecessors to construct actions for the path for a given round
+        round_predecessors = [ {} for _ in range(self.max_rounds+1) ]
+
+        # stops that were traveled to on the previous round
+        marked_stops = set()
+        marked_stops.add(start_pos)
+
+        # inside get_path, before your main loop
+        arrival_round: dict[tuple,int] = {}   # (x,y) → the round it was first reached
+        arrival_round[start_pos] = 0
+
+        for round_num in range(1, self.max_rounds + 1):
+
+            updated_stops = set()
+
+            for pos in marked_stops:
+
+                x, y = pos
+                node = grid.get_from_grid(x, y)
+
+                if not node.is_station:
+                    continue
+
+                for route in [grid.horizontal_routes[y], grid.vertical_routes[x]]:
+                    # Forward scan of this route
+
+                    curr_time = arrival_times[round_num - 1][pos]
+
+                    neighbors_on_route = [st for st in node.neighbor_stations if st in route.stations and st is not node]
+
+                    # simulate arrival times
+                    for nbr in neighbors_on_route:
+
+                        dep_time = curr_time + route.time_until_bus(node, curr_time)
+                        arr_time = dep_time + route.time_until_bus(nbr, dep_time)
+
+                        to_pos = (nbr.pos_X, nbr.pos_Y)
+
+                        # print("Found nbr: " + str(to_pos) + " departs at " + str(dep_time) + " arrives at " + str(arr_time))
+
+                        if arr_time < arrival_times[round_num][to_pos]:
+
+                            if to_pos not in arrival_round:
+                                arrival_round[to_pos] = round_num
+
+                            arrival_times[round_num][to_pos] = arr_time
+                            round_predecessors[round_num][to_pos] = (pos, dep_time, arr_time)
+                            # print("Setting pred of " + str(to_pos) + " as " + str(pos))
+                            updated_stops.add(to_pos)
+
+            # print("New pred: " + str(pred))
+
+            # print("Round num: " + str(round_num))
+
+            # print("Round's arrival times: " + str(arrival_times[round_num]))
+
+            # print("Updated stops: " + str(updated_stops))
+
+            if goal_pos in updated_stops:
+                break
+
+            marked_stops = updated_stops
+
+            # if no stops are updated, end early
+            if not updated_stops:
+                break
+
+        # print(str(predecessors))
+
+        actions = []
+        cur_pos = goal_pos
+        r = arrival_round[cur_pos]
+
+        goal_arrive_time = arrival_times[r][goal_pos]
+
+        while r > 0:
+            prev_pos, dep_time, arr_time = round_predecessors[r][cur_pos]
+
+            actions.append((arr_time, "unboard"))
+
+            if prev_pos[0] == cur_pos[0]:
+                actions.append((dep_time, "boardVert"))
+            else:
+                actions.append((dep_time, "boardHoriz"))
+
+            #print(actions)
+
+            cur_pos = prev_pos
+            r = arrival_round[cur_pos]
+
+        actions.reverse()
+        return actions, goal_arrive_time
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from algorithms import *
 from transit import *
 import time
-import os
-import sys
-
-def clear_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 def run_algo(algo: Algorithm, grid: TransitGrid): # TODO
 
@@ -34,7 +29,7 @@ def run_algo(algo: Algorithm, grid: TransitGrid): # TODO
     after_time = time.time()
 
     # returns elapsed time
-    return after_time - before_time, total_sim_time
+    return after_time - before_time, total_sim_time, len(actions)/2
 
 def run_full_test(repetitions = 1000, grid_size = 10, express_chance = 0.2, random_bus_start=True, n_stations = 50): #TODO: this should run many tests at the same time
 
@@ -44,53 +39,55 @@ def run_full_test(repetitions = 1000, grid_size = 10, express_chance = 0.2, rand
 
     times_seconds = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
 
-    max_sim_times = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
+    sum_sim_times = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
+
+    sum_transfers = {"dijkstras": 0.0, "raptor": 0.0, "connection": 0.0 }
 
     for i in range(repetitions):
 
         seed = random.randint(0, 100000)
 
         grid_dij = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
-        
-        # print(grid_dij)
 
-        elapsed_time_dij, sim_time_dij = run_algo(dijkstra, grid_dij)
+        elapsed_time_dij, sim_time_dij, tfrs_dij = run_algo(dijkstra, grid_dij)
         
         times_seconds["dijkstras"] += elapsed_time_dij
-        max_sim_times["dijkstras"] = max(max_sim_times["dijkstras"], sim_time_dij)
+        sum_sim_times["dijkstras"] += sim_time_dij
+        sum_transfers["dijkstras"] += tfrs_dij
 
         grid_rapt = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
-        times_seconds["raptor"] += run_algo(raptor, grid_rapt)
+        
+        elapsed_time_rapt, sim_time_rapt, tfrs_rapt = run_algo(raptor, grid_rapt)
 
-        # TODO: uncomment once implemented
+        times_seconds["raptor"] += elapsed_time_rapt
+        sum_sim_times["raptor"] += sim_time_rapt
+        sum_transfers["raptor"] += tfrs_rapt
+
         grid_conn = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=random_bus_start, seed=seed, n_stations=n_stations)
         
-        elapsed_time_conn, sim_time_conn = run_algo(connection, grid_conn)
+        elapsed_time_conn, sim_time_conn, tfrs_conn = run_algo(connection, grid_conn)
         times_seconds["connection"] += elapsed_time_conn
-        max_sim_times["connection"] = max(sim_time_conn, max_sim_times["connection"])
+        sum_sim_times["connection"] += sim_time_conn
+        sum_transfers["connection"] += tfrs_conn
 
         if sim_time_dij != sim_time_conn:
             raise ValueError("Values should be equal for optimal solutions")
 
-    return times_seconds, max_sim_times
+    return times_seconds, {k:(v/repetitions) for (k,v) in sum_sim_times.items()}, {k:(v/repetitions) for (k,v) in sum_transfers.items()}
 
 # small grid size
 
-repetitions = 1000
-grid_size = 50
+repetitions = 100
+grid_size = 20
 express_chance = 0.2
 n_stations = grid_size*grid_size // 2
 
-raptor = RaptorAlgorithm()
-grid_rapt = TransitGrid(grid_size, express_chance=express_chance, random_bus_start=True, seed=random.randint(0, 100000), n_stations=n_stations)
-run_algo(raptor, grid_rapt)
-sys.exit(0)
-
-test_results, max_sim_times = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
+test_results, avg_sim_times, avg_transfers = run_full_test(repetitions=repetitions, grid_size=grid_size, express_chance=express_chance, n_stations=n_stations)
 print("Algorithm timing results for " + str(repetitions) + " repetitions of a " + str(grid_size) + "x" + str(grid_size) 
       + " grid with a " + str(express_chance) + " probability of express lines and " + str(n_stations) + " stations:")
-print(test_results)
-print(max_sim_times)
+print("Total compute time: " + str(test_results))
+print("Average timesteps to goal: " + str(avg_sim_times))
+print("Average transfers to goal: " + str(avg_transfers))
 
 # medium grid size
 
@@ -117,44 +114,3 @@ print(max_sim_times)
 #       + " grid with a " + str(express_chance) + " probability of express lines and " + str(n_stations) + " stations:")
 # print(test_results)
 # print(max_sim_times)
-
-# UNCOMMENT TO TEST DIJKSTRAS INCLUDING PRINTING INTERMEDIATE GRIDS
-# dij = DijkstraAlgorithm()
-
-# before_time = time.time()
-
-# for i in range(5):
-#     grid = TransitGrid(100, express_chance=0.2, random_bus_start=True, seed = -1, n_stations = 500)
-#     actions, total_time = dij.get_path(grid)
-
-#     if actions == None:
-#         raise RuntimeError("No path found!")
-
-#     # print(grid)
-
-#     action_index = 0
-
-#     while(True):
-
-#         #clear_console()
-#         #print(grid)
-#         #print(actions)
-
-#         # execute actions
-#         while action_index < len(actions) and actions[action_index][0] <= grid.time:
-#             action_time, action = actions[action_index]
-#             grid.take_action(action)
-#             action_index += 1
-#             #time.sleep(5)
-
-#         #time.sleep(0.5)
-#         grid.step()
-
-#         if grid.passenger_at_goal():
-#             break
-
-# after_time = time.time()
-
-# elapsed_seconds = after_time - before_time
-
-# print("Successfully simulated 1,000 dijkstras! Took " + str(elapsed_seconds) + " seconds")
